afce834da6ba2bf02ffd0067cbd911074ce2c1a9
h@di <hadi@hadisafari.ir>
2018-07-27 16:51:49 +0430

    add Crawler class

--- __main__.py
+++ __main__.py
@@ -8,58 +8,72 @@ from db_ctrl import DBCtrl
 from gitlab_ctrl import GitlabCtrl
 
 
-def read_configs(config_file, status_file):
-    """Read global configurations and status"""
-    status = {}
-    try:
-        with open(status_file) as f:
-            status = json.load(f)
-    except Exception as ex:
-        print("Status file (%s) error: %s\nUse default status.\n" % (status_file, ex), file=stderr, flush=True)
-    try:
-        with open(config_file) as f:
-            conf = json.load(f)
-            phases = conf['phases']
-            if not status:
-                status = conf['default_status']
-    except Exception as ex:
-        print("Config file (%s) error: %s\n" % (config_file, ex), file=stderr, flush=True)
-        exit(1)
-    return (status, phases)
+class Crawler(object):
+    """Main crawler class"""
+    status_file = str(Path.home()) + '/.glc'
+    config_file = 'config.json'
+
+    def __init__(self):
+        self._read_configs()
+        self.db_ctrl = DBCtrl()
+        self.gitlab = GitlabCtrl()
+
+    def _read_configs(self):
+        """Read global configurations and status"""
+        self.status = {}
+        try:
+            with open(self.status_file) as f:
+                self.status = json.load(f)
+        except Exception as ex:
+            print("Status file (%s) error: %s\nUse default status.\n" % (self.status_file, ex), file=stderr, flush=True)
+        try:
+            with open(self.config_file) as f:
+                conf = json.load(f)
+                self.phases = conf['phases']
+                if not self.status:
+                    self.status = conf['default_status']
+        except Exception as ex:
+            print("Config file (%s) error: %s\n" % (self.config_file, ex), file=stderr, flush=True)
+            exit(1)
+
+    def _add_project_to_db(self, project):
+        """Add data of a project to database."""
+        self.db_ctrl.add_row('projects', {
+            "id": project['id'],
+            "path": project['path'],
+            "owner_path": project['path_with_namespace'].split('/')[0],
+            "display_name": project['name'],
+            "description": project['description'],
+            "avatar": project['avatar_url'],
+            "stars": project['star_count'],
+            "forks": project['forks_count'],
+            "last_activity": project['last_activity_at'][:-1]
+        })
+
+    def run(self):
+        """Run crawler."""
+        try:
+            if self.phases.get('get_all_projects', False):
+                self.gitlab.process_all_projects(
+                    self._add_project_to_db,
+                    {'archived': True},
+                    start_page=self.status['get_all_projects_start_page']
+                )
+
+        except KeyboardInterrupt as ex:
+            print("KeyboardInterrupt")
+        except ...:
+            raise
+        finally:
+            try:
+                with open(self.status_file, 'w') as f:
+                    print(json.dumps(self.status, indent=4), file=f)
+            except Exception as ex:
+                print("Saving status file (%s) error: %s\n" % (self.status_file, ex), file=stderr, flush=True)
 
 
 def main():
-    status_file = str(Path.home()) + '/.glc'
-
-    try:
-        (status, phases) = read_configs('config.json', status_file)
-        db_ctrl = DBCtrl()
-        gitlab = GitlabCtrl()
-
-        if phases.get('get_all_projects', False):
-            gitlab.process_all_projects(
-                lambda project: db_ctrl.add_row('projects', {
-                    "id": project['id'],
-                    "path": project['path'],
-                    "owner_path": project['path_with_namespace'].split('/')[0],
-                    "display_name": project['name'],
-                    "description": project['description'],
-                    "avatar": project['avatar_url'],
-                    "stars": project['star_count'],
-                    "forks": project['forks_count'],
-                    "last_activity": project['last_activity_at'][:-1]
-                }), {'archived': True}, start_page=status['get_all_projects_start_page'])
-
-    except KeyboardInterrupt as ex:
-        print("KeyboardInterrupt")
-    except ...:
-        raise
-    finally:
-        try:
-            with open(status_file, 'w') as f:
-                print(json.dumps(status, indent=4), file=f)
-        except Exception as ex:
-            print("Saving status file (%s) error: %s\n" % (status_file, ex), file=stderr, flush=True)
+    Crawler().run()
 
 
 if __name__ == '__main__':
