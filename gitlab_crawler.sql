5daef85f5225419b93a747687da63ecac11d1cf9
H@di <hadi@hadisafari.ir>
2018-08-04 18:42:44 +0000

    add forks table

--- gitlab_crawler.sql
+++ gitlab_crawler.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d8d7f033797a59c43f0e6cf841dcb2d6aba91b6b409633aac857803e82a68a5
-size 125277599
+oid sha256:7e30415fe3a7fdf47618d0c91b7aba696c099f237e5583cffd28a1fcac61adb6
+size 126981762
