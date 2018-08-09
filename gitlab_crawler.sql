086071d3c8fca4ded56c28176a8c1bf680709277
H@di <hadi@hadisafari.ir>
2018-08-09 16:50:38 +0000

    add users for projects with stars or forks >= 10

--- gitlab_crawler.sql
+++ gitlab_crawler.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ced47052e85ab775bc87c0db4336c7528f03067d081d3b249c527e4d403007b0
-size 129974322
+oid sha256:2453acfeb81846f53d3c3cb4a10aa64f0f3eefa9337ec9aaa73d2eb92b903431
+size 131808041
